316b3f0cc0be672fcf695043decfaac0a6ef0cab
Andrey-Glukhov <agi2000@mail.ru>
2021-03-15 16:23:10 +0300

    Add woocommerce support to theme

--- functions.php
+++ functions.php
@@ -22,3 +22,8 @@ add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-formats', array('aside', 'chat', 'gallery','link','image','quote','status','video'));
 add_theme_support('post-thumbnails');
+//Add woocommerce support to theme
+function mytheme_add_woocommerce_support() {
+  add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
